22e985b00d39ea541a4f0506af32750c45922783
julio7173 <juliocesarsevericheorellana@gmail.com>
2023-08-24 14:43:32 -0400

    Login funcionando

---
+++ js/formulario.js
@@ -0,0 +1,71 @@
+// Seleccionar los elementos del formulario
+var formulario = document.querySelector(".formulario");
+var nombre = document.querySelector(".formulario__nombre");
+var mensaje = document.querySelector(".formulario__mensaje");
+var boton = document.querySelector(".formulario__boton");
+
+// Agregar un evento de submit al formulario
+formulario.addEventListener("submit", function (event) {
+  // Evitar que el formulario se envíe por defecto
+  event.preventDefault();
+  // Limpiar los posibles mensajes de error anteriores
+  limpiarErrores();
+  // Validar los campos del formulario
+  var errores = validarCampos(nombre.value, mensaje.value);
+  // Si hay errores, mostrarlos al usuario
+  if (errores.length > 0) {
+    mostrarErrores(errores);
+  } else {
+    // Si no hay errores, enviar el mensaje
+    enviarMensaje(nombre.value, mensaje.value);
+  }
+});
+
+// Función que valida los campos del formulario y devuelve un array de errores
+function validarCampos(nombre, mensaje) {
+  var errores = [];
+  // Validar que el nombre no esté vacío y tenga máximo 40 caracteres
+  if (nombre.trim() == "") {
+    errores.push("El nombre no puede estar vacío");
+  } else if (nombre.length > 40) {
+    errores.push("El nombre no puede tener más de 40 caracteres");
+  }
+  // Validar que el mensaje no esté vacío y tenga máximo 120 caracteres
+  if (mensaje.trim() == "") {
+    errores.push("El mensaje no puede estar vacío");
+  } else if (mensaje.length > 120) {
+    errores.push("El mensaje no puede tener más de 120 caracteres");
+  }
+  return errores;
+}
+
+// Función que muestra los mensajes de error al usuario
+function mostrarErrores(errores) {
+  // Crear un elemento ul para contener los mensajes de error
+  var ul = document.createElement("ul");
+  ul.className = "formulario__errores";
+  // Recorrer el array de errores y crear un elemento li por cada uno
+  for (var i = 0; i < errores.length; i++) {
+    var li = document.createElement("li");
+    li.textContent = errores[i];
+    ul.appendChild(li);
+  }
+  // Insertar el elemento ul antes del botón del formulario
+  formulario.insertBefore(ul, boton);
+}
+
+// Función que limpia los mensajes de error anteriores
+function limpiarErrores() {
+  // Seleccionar el elemento ul que contiene los mensajes de error
+  var ul = document.querySelector(".formulario__errores");
+  // Si existe, eliminarlo del formulario
+  if (ul) {
+    formulario.removeChild(ul);
+  }
+}
+
+// Función que muestra una alerta al usuario con el nombre y el mensaje
+function enviarMensaje(nombre, mensaje) {
+  // Mostrar una alerta con el nombre y el mensaje
+  alert("Tu nombre es " + nombre + " y tu mensaje es " + mensaje + ". Gracias por contactarnos.");
+}
--- js/login.js
+++ js/login.js
@@ -1,3 +1,62 @@
-function irLogin(link){
+function irLogin(link) {
     location.href = link.value;
 }
+
+// Crear un array de objetos con los correos y contraseñas válidas
+let usuarios = [
+  {correo: "admin1@gmail.com", password: "1234"},
+  {correo: "admin2@gmail.com", password: "5678"},
+  {correo: "admin3@gmail.com", password: "9012"}
+];
+
+// Obtener el formulario por su clase
+let formulario = document.querySelector(".login__formulario");
+
+// Obtener los elementos span por su clase
+let errorCorreo = document.querySelector(".login__email + .error");
+let errorPassword = document.querySelector(".login__password + .error");
+
+// Agregar un evento de tipo submit al formulario
+formulario.addEventListener("submit", function(evento) {
+  // Evitar que el formulario se envíe sin validar
+  evento.preventDefault();
+
+  // Obtener los valores de los campos de correo y contraseña
+  let correo = formulario.correo.value;
+  let password = formulario.password.value;
+
+  // Crear una expresión regular para validar el formato del correo
+  let regex = /\w+@\w+\.\w+/;
+
+  // Verificar que los campos no estén vacíos
+  if (correo === "") {
+    errorCorreo.textContent = "Por favor, llene el campo correo";
+    return;
+  }
+
+  if (password === "") {
+    errorPassword.textContent = "Por favor, llene el campo constraseña";
+    return;
+  }
+
+  // Verificar que el correo tenga el formato correcto
+  if (!regex.test(correo)) {
+    errorCorreo.textContent = "Por favor, ingrese un correo válido";
+    return;
+  } else {
+    errorCorreo.textContent = "";
+  }
+
+  // Buscar si el correo y la contraseña coinciden con algún elemento del array de usuarios
+  let usuario = usuarios.find(function(elemento) {
+    return elemento.correo === correo && elemento.password === password;
+  });
+
+  // Si hay una coincidencia, redirigir al menú de administrador
+  if (usuario) {
+    window.location.href = "/admin.html";
+  } else {
+    // Si no hay una coincidencia, mostrar un mensaje de error en el campo de contraseña
+    errorPassword.textContent = "Correo o contraseña incorrectos";
+  }
+});
